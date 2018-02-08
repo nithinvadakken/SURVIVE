ec6f64001f5f32b816d2a206a2c5b63378c68853
wikto <wiktorlidwin@gmail.com>
2018-02-08 11:43:35 -0500

    Size + no more spawning on top of u

--- EscapeJS.js
+++ EscapeJS.js
@@ -62,7 +62,7 @@ var ax = getRandomInt(90, canvas.width-30);
 var ay = getRandomInt(90, canvas.height-30);
 var maxlevel = 20;
 var timer = 0;
-var hero_size = 1/9 * canvas.width;
+var hero_size = 1/1200 * canvas.width * canvas.height;
 
 
 // spawn
