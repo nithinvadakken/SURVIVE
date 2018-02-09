499a69210a3c378891f28aa0d3ea6bee145dee12
Jack Lee <jack200336@gmail.com>
2018-02-09 07:51:04 -0500

    multiple people

--- EscapeJS.js
+++ EscapeJS.js
@@ -1,3 +1,51 @@
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function indexOf(member, startFrom) {
+    /*
+    In non-strict mode, if the `this` variable is null or undefined, then it is
+    set to the window object. Otherwise, `this` is automatically converted to an
+    object. In strict mode, if the `this` variable is null or undefined, a
+    `TypeError` is thrown.
+    */
+    if (this == null) {
+      throw new TypeError("Array.prototype.indexOf() - can't convert `" + this + "` to object");
+    }
+
+    var
+      index = isFinite(startFrom) ? Math.floor(startFrom) : 0,
+      that = this instanceof Object ? this : new Object(this),
+      length = isFinite(that.length) ? Math.floor(that.length) : 0;
+
+    if (index >= length) {
+      return -1;
+    }
+
+    if (index < 0) {
+      index = Math.max(length + index, 0);
+    }
+
+    if (member === undefined) {
+      /*
+        Since `member` is undefined, keys that don't exist will have the same
+        value as `member`, and thus do need to be checked.
+      */
+      do {
+        if (index in that && that[index] === undefined) {
+          return index;
+        }
+      } while (++index < length);
+    } else {
+      do {
+        if (that[index] === member) {
+          return index;
+        }
+      } while (++index < length);
+    }
+
+    return -1;
+  };
+}
+
+
 //asks for username
 nametag = prompt("\"NEW UPDATE PRESS \"P\" FOR PAUSE \nPlease enter an appropriate user name:");
 
@@ -23,12 +71,15 @@ function getCookie(cookiename) {
 
 
 function checkCookie() {
-    var name = getCookie("username");
-    if (name === nametag) {
-        alert("Back for more, " + nametag + "?");
+    var temp = getCookie("usernames")||"[]";
+    console.log(temp);
+    var names = JSON.parse(temp);
+    if (names.indexOf(nametag) != -1) {
+        alert("Back for more, " + names[names.indexOf(nametag)] + "?");
     } else {
         if (nametag != "" && nametag != null) {
-            setCookie("username", nametag, 365);
+            names.push(nametag);
+            setCookie("usernames", JSON.stringify(names), 365);
         }
     }
 }
@@ -170,30 +221,35 @@ function keyUpHandler(e) {
 function charDraw() {
     ctx.beginPath();
     ctx.fillStyle = "orange";
-    ctx.fillRect(x, y, hero_size, hero_size);
-
+    ctx.fillRect(x, y, 30, 30);
     ctx.font = "15px Arial";
-    ctx.textAlign = "center";
     if (y - 50 < 0) {
         if (x - 10 < 0) {
-            ctx.fillText(nametag, x + 50, y + 60);
+            ctx.textAlign = "left";
+            ctx.fillText(nametag, x, y + 60);
         }
         else if (x + 50 > canvas.width) {
-            ctx.fillText(nametag, x - 50, y + 60);
+            ctx.textAlign = "right";
+            ctx.fillText(nametag, x + 25, y + 60);
         }
         else if (x - 10 > 0 && x + 20 < canvas.width) {
+            ctx.textAlign = "center";
             ctx.fillText(nametag, x + 15, y + 60);
         }
     }
     else if (x + 50 > canvas.width) {
-        ctx.fillText(nametag, x - 50, y - 30);
+        ctx.textAlign = "right";
+        ctx.fillText(nametag, x + 25, y - 30);
     }
     else if (x - 10 < 0) {
-        ctx.fillText(nametag, x + 50, y - 30);
+        ctx.textAlign = "left";
+        ctx.fillText(nametag, x, y - 30);
     }
     else {
+        ctx.textAlign = "center";
         ctx.fillText(nametag, x + 15, y - 30);
     }
+    ctx.textAlign = "center";
 
 }
 
